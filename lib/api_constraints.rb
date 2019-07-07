440678ba868359e5734e64718c64fb09eba983c6
linkoad <danteabreu1@gmail.com>
2019-07-07 18:46:11 +0000

    blog_place+api

---
+++ lib/api_constraints.rb
@@ -0,0 +1,11 @@
+
+class ApiConstraints
+  def initialize(options)
+    @version = options[:version]
+    @default = options[:default]
+  end
+
+  def matches?(req)
+    @default || (req.respond_to?('headers') && req.headers.key?('Accept') && req.headers['Accept'].include?("application/vnd.blog_place.v#{@version}"))
+  end
+end
