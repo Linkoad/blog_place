af3e7c48579d662d5696d96ada26b7c0a550d5d0
linkoad <danteabreu1@gmail.com>
2019-07-30 21:08:40 +0000

    remove conflict

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -23,7 +23,11 @@ describe Api::V1::UsersController do
 
     context "when is successfully created" do
       before(:each) do
+<<<<<<< HEAD
         @user_attributes = FactoryBot.attributes_for :user
+=======
+        @user_attributes = FactoryBot.attributes_for(:user)
+>>>>>>> 295650a67ca231ab31fb354acb19ab54d28d564a
         post :create, params: { user: @user_attributes }, format: :json
       end
 
@@ -37,12 +41,18 @@ describe Api::V1::UsersController do
 
     context "when is not created" do
       before(:each) do
+<<<<<<< HEAD
         # notice I'm not including the email
         @invalid_user_attributes = { password: "12345678",
 
                                      password_confirmation: "12345678" }
         post :create, params: { user: @invalid_user_attributes },
           format: :json
+=======
+        @invalid_user_attributes = FactoryBot.invalid_user_attributes_for { password: '12345678',
+                                     password_confirmation: '12345678' }
+        post :create, { user: @invalid_user_attributes }, format: :json
+>>>>>>> 295650a67ca231ab31fb354acb19ab54d28d564a
       end
 
       it "renders an errors json" do
